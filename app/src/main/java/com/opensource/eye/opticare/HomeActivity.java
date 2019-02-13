d60de28af171276cb6eefe812ae3f03b9c33e244
AchalaKavinda <achalakavinda25r@gmail.com>
2019-02-14 00:06:17 +0530

    add home interface

--- app/src/main/java/com/opensource/eye/opticare/HomeActivity.java
+++ app/src/main/java/com/opensource/eye/opticare/HomeActivity.java
@@ -1,13 +1,28 @@
 package com.opensource.eye.opticare;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+
 public class HomeActivity extends AppCompatActivity {
+
+    BoomMenuButton bmb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bmb = (BoomMenuButton) findViewById(R.id.bmb);
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            SimpleCircleButton.Builder builder = new SimpleCircleButton.Builder()
+                    .normalImageRes(R.drawable.person);
+            bmb.addBuilder(builder);
+            bmb.setRippleEffect(true);
+            bmb.setNormalColor(getResources().getColor(R.color.gradStop));
+        }
     }
 }
--- app/src/main/java/com/opensource/eye/opticare/MainActivity.java
+++ app/src/main/java/com/opensource/eye/opticare/MainActivity.java
@@ -1,20 +1,23 @@
 package com.opensource.eye.opticare;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    RelativeLayout rellay1;
+    RelativeLayout relativeLayout_1;
+    Button buttonLogin;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            rellay1.setVisibility(View.VISIBLE);
+            relativeLayout_1.setVisibility(View.VISIBLE);
         }
     };
 
@@ -24,8 +27,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        relativeLayout_1 = (RelativeLayout) findViewById(R.id.relativeLayout_1);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(this);
 
         handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonLogin:
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
